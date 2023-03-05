c2c6d72ca9ea6040ee47b0e0d13ee47e0f2e9af6
Bruno Neves <bnsouzas@gmail.com>
2023-03-05 13:07:37 +0100

    generate krakend config

---
+++ krakend/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+rm -rf krakend/krakend.json
+if [ -f "./.env" ]; then
+    source ./.env
+fi
+sed "s/_GOOGLE_CLOUD_PROJECT/${PROJECT_ID}/g" krakend/krakend.tpl.json | \
+sed "s|_IRON_BANK_HOST_URL|${_IRON_BANK_HOST_URL}|g" | \
+sed "s|_WALL_REGISTER_HOST_URL|${_WALL_REGISTER_HOST_URL}|g" | \
+sed "s|_AUTH_JWK_URL|${_AUTH_JWK_URL}|g" | \
+sed "s|_AUTH_AUDIENCE|${_AUTH_AUDIENCE}|g" | \
+sed "s|_AUTH_SSO_AUDIENCE|${_AUTH_SSO_AUDIENCE}|g" | \
+sed "s/_PROJECT_REGION/$$PROJECT_REGION/g" > krakend/krakend.json
